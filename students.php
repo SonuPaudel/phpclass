ee66d41b7bd753c1b01d9708ca705db65f320e1a
Sonu Paudel <86399801+SonuPaudel@users.noreply.github.com>
2023-02-23 09:53:08 +0545

    Initial commit

---
+++ students.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Students</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <h2 class="text-center my-3 font-bold text-3xl">Our Students</h2>
+    <div class="px-36 grid grid-cols-4 gap-8">
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+        <div class="mt-2 bg-gray-200 p-5 rounded-lg shadow">
+            <p>ID: 1</p>
+            <p>Name: Ram</p>
+            <p>Phone: 9800725710</p>
+            <p>Address: Gaindakot</p>
+            <p>Email: ritesh@lict.edu.np</p>
+            <p>Faculty: BIM</p>
+
+        </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
